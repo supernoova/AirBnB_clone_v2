ef6c1690b88c81361e041f50fa8f293e8bd759be
supernoova <abdellatiffitah@gmail.com>
2024-07-11 16:43:55 -0700

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,91 @@
-# puppet to config the server
-exec { 'update server':
-  command => '/usr/bin/apt -y update',
-}
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 package { 'nginx':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
-exec { 'update server':
-  command => '/usr/bin/mkdir -p /data/web_static/releases/test/',
-}
-exec { 'update server':
-  command => '/usr/bin/mkdir -p /data/web_static/shared/',
-}
-exec { 'update server':
-  command => '/usr/bin/echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo /usr/bin/tee /data/web_static/releases/test/index.html',
-}
-exec { 'update server':
-  command => '/usr/bin/ln -sf /data/web_static/releases/test/ /data/web_static/current',
-}
-exec { 'update server':
-  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
-}
-exec { 'update server':
-  command => 'sudo sed -i "/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-exec { 'update server':
-  command => '/usr/bin/service nginx restart',
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
